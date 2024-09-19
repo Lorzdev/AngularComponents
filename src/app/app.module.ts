0b07377bb8e0352200927a608564d9b0b3dbfa48
lorenz <lorenzcamo93@gmail.com>
2024-09-19 20:18:24 +0800

    10th commit add currencyconverter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UserageComponent } from "./components/userage/userage.component";
 import {UsergreetingComponent} from "./components/usergreeting/usergreeting.component";
 import {CalculatorComponent} from "./components/calculator/calculator.component";
 import {TextlengthComponent} from "./components/textlength/textlength.component";
+import {CurrencyconverterComponent} from "./components/currencyconverter/currencyconverter.component";
 
 
 const routes: Routes = [
@@ -26,6 +27,7 @@ const routes: Routes = [
   {path:  'usergreeting', component: UsergreetingComponent},
   {path:  'calculator', component: CalculatorComponent},
   {path:  'textlength', component: TextlengthComponent},
+  {path:  'currencyconverter', component: CurrencyconverterComponent},
 
 
 
@@ -55,6 +57,7 @@ const routes: Routes = [
     UsergreetingComponent,
     CalculatorComponent,
     TextlengthComponent,
+    CurrencyconverterComponent,
 
   ],
 
