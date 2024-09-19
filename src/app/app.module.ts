347366d87c81d02b94fd1547982f4beca5c1cf02
lorenz <lorenzcamo93@gmail.com>
2024-09-20 00:29:32 +0800

    20th commit add palindromechecker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import {SimpleloginComponent} from "./components/simplelogin/simplelogin.compone
 import {FahrenheittocelsiusComponent} from "./components/fahrenheittocelsius/fahrenheittocelsius.component";
 import {BookmarklistComponent} from "./components/bookmarklist/bookmarklist.component";
 import {CharactercounterComponent} from "./components/charactercounter/charactercounter.component";
+import {PalindromecheckerComponent} from "./components/palindromechecker/palindromechecker.component";
 
 
 const routes: Routes = [
@@ -46,6 +47,7 @@ const routes: Routes = [
   {path:  'fahrenheittocelsius', component:FahrenheittocelsiusComponent},
   {path:  'bookmarklist', component:BookmarklistComponent},
   {path:  'charactercounter', component:CharactercounterComponent},
+  {path:  'palindromechecker', component:PalindromecheckerComponent},
 
 
 
@@ -89,6 +91,7 @@ const routes: Routes = [
     FahrenheittocelsiusComponent,
     BookmarklistComponent,
     CharactercounterComponent,
+    PalindromecheckerComponent,
 
   ],
 
